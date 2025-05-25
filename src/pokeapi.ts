db5ac43ade4f3b28c637d9b9b671b6a2b0865131
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-05-25 20:27:00 +0100

    Added fetch requests for shallow locations and locations

--- src/pokeapi.ts
+++ src/pokeapi.ts
@@ -4,17 +4,37 @@ export class PokeAPI {
     constructor() {}
 
     async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+        if (!pageURL){
+            const response = await fetch(`${PokeAPI.baseURL}`);
+        }
+
+        const response = await fetch(`${pageURL}`);
+        
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data: ShallowLocations = await response.json();
+        return data
 
     }
 
     async fetchLocation(locationName: string): Promise<Location> {
+        const response = await fetch(`${PokeAPI.baseURL}/${locationName}`);
 
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data: Location = await response.json();
+        return data
     }
 
     
 }
 
-export type NameUrl = { name: string; url: string;}
+    export type NameUrl = { name: string; url: string;}
     
     export type ShallowLocations = {
         next: string | null;
@@ -25,5 +45,82 @@ export type NameUrl = { name: string; url: string;}
     
 
     export type Location = {
-
+        encounter_method_rates: EncounterMethodRate[]
+        game_index: number
+        id: number
+        location: NameUrl
+        name: string
+        names: Name[]
+        pokemon_encounters: PokemonEncounter[]
     };
+
+    // MARK - : location types
+
+    export interface EncounterMethodRate {
+        encounter_method: EncounterMethod
+        version_details: VersionDetail[]
+    }
+
+    export interface EncounterMethod {
+        name: string
+        url: string
+    }
+
+    export interface VersionDetail {
+        rate: number
+        version: Version
+    }
+
+    export interface Version {
+        name: string
+        url: string
+    }
+
+    export interface Name {
+        language: Language
+        name: string
+    }
+
+    export interface Language {
+        name: string
+        url: string
+    }
+
+    export interface PokemonEncounter {
+        pokemon: Pokemon
+        version_details: VersionDetail2[]
+    }
+
+    export interface Pokemon {
+        name: string
+        url: string
+    }
+
+    export interface VersionDetail2 {
+        encounter_details: EncounterDetail[]
+        max_chance: number
+        version: Version2
+    }
+
+    export interface EncounterDetail {
+        chance: number
+        condition_values: ConditionValue[]
+        max_level: number
+        method: Method
+        min_level: number
+    }
+
+    export interface ConditionValue {
+        name: string
+        url: string
+    }
+
+    export interface Method {
+        name: string
+        url: string
+    }
+
+    export interface Version2 {
+        name: string
+        url: string
+    }
