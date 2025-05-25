b341cf0526ed96e234fcc213ef2539943d60a432
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-05-25 18:58:31 +0100

    Adjusted commandhelp function output

--- src/command_help.ts
+++ src/command_help.ts
@@ -3,6 +3,9 @@ import { getCommands } from "./commands.js";
 export function commandHelp(){
     const commandRegistry = getCommands();
 
+    console.log("Welcome to the Pokedex!")
+    console.log("Usage:")
+    console.log("")
     for (let command of Object.values(commandRegistry)) {
         console.log(`${command.name}: ${command.description}`)
     }
