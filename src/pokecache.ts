e35216ae95d9f5d0a4d63ab71105e75a689159e2
Taijaun Pitt <taijaun@hotmail.co.uk>
2025-05-26 11:19:05 +0100

    Added startReapLoop function

--- src/pokecache.ts
+++ src/pokecache.ts
@@ -36,4 +36,10 @@ export class Cache {
         }
     }
 
+    #startReapLoop() {
+        this.#reapIntervalId = setInterval(() => {
+            this.#reap();
+        }, this.#interval);
+    }
+
 }
